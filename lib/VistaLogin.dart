036ef7593b4bcb8eea86de10bb87f0e67e20ff01
Abraham Espinoza <espinoza.sistemas@gmail.com>
2020-03-20 18:58:02 -0500

    Mamalon

--- lib/VistaLogin.dart
+++ lib/VistaLogin.dart
@@ -5,10 +5,34 @@ class vistaLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text("Que onda perro maldito"),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/fondo.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Text("Boost life", style: TextStyle(
+                  fontSize: 40.0, fontFamily: 'PacificoRegular',),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              Container(child: Text(
+                  "Registrarte y olvidate de los alimentos caducados", style: TextStyle(fontFamily: 'NarrowRegular',),),
+              ),
+            ],
+          ),
         ),
-      ),
+        ),
     );
   }
 }
+
+//Text("Boost life", style: TextStyle(fontSize: 30.0, fontFamily: 'PacificoRegular',)
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:loading_animations/loading_animations.dart';
 import 'package:boostapp/VistaLogin.dart';
 import 'dart:ui';
-
+import 'package:async/async.dart';
 void main() => runApp(VistaPresentacion());
 
 class VistaPresentacion extends StatefulWidget {
@@ -11,54 +13,59 @@ class VistaPresentacion extends StatefulWidget {
 }
 
 class _VistaPresentacionState extends State<VistaPresentacion> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
+  Widget wActual = Landing();
   @override
   Widget build(BuildContext context) {
-    var future = new Future.delayed(const Duration(milliseconds: 5400), () {
-      print("Test");
+     var future2 = new Future.delayed(const Duration(milliseconds: 4100), (){
+      setState(() {
+        wActual = vistaLogin();
+      });
     });
     return MaterialApp(
       title: 'Bootslife',
       theme: ThemeData(),
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/fondo.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: Image.asset(
-                  "assets/boostlife.png",
-                  width: 250.0,
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Center(
-                child: LoadingBouncingGrid.circle(
-                  borderColor: Color(0xFF55b85a),
-                  borderSize: 1.0,
-                  size: 50.0,
-                  backgroundColor: Color(0xFF55b85a),
-                  duration: Duration(milliseconds: 2000),
-                  inverted: true,
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: wActual,
       ),
     );
   }
 }
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/fondo.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              "assets/boostlife.png",
+              width: 250.0,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Center(
+            child: LoadingBouncingGrid.circle(
+              borderColor: Color(0xFF55b85a),
+              borderSize: 1.0,
+              size: 50.0,
+              backgroundColor: Color(0xFF55b85a),
+              duration: Duration(milliseconds: 2000),
+              inverted: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
